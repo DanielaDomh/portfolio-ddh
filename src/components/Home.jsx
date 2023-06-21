4eb46a501dbec37bc09d2e5ea0fb8433d93cc9c5
Daniela Domínguez <129920089+DanielaDomh@users.noreply.github.com>
2023-06-20 19:59:56 -0600

    sobremi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,31 +10,57 @@ const Home = () => {
             <h1>Hola! 👋🏻 Soy Daniela Domínguez</h1>
             <p>Desarrolladora Web Full Stack especializado en Front End</p>
             <div>
-              <a href="https://www.linkedin.com/in/daniela-hern%C3%A1ndez-b96550210/" target="_blank">
+              <a
+                href="https://www.linkedin.com/in/daniela-hern%C3%A1ndez-b96550210/"
+                target="_blank"
+              >
                 <button className="button-link">Linkedln</button>
               </a>
               <a href="https://github.com/DanielaDomh" target="_blank">
-              <button className="button-git">Github</button>
+                <button className="button-git">Github</button>
               </a>
             </div>
           </div>
         </article>
         <article id="aboutMe">
-          <div>
-            <p>Sobre Mi</p>
+          <h2>Sobre Mi</h2>
+          <div className="about-me">
             <p>
-              Hola mi nombre es Daniela, tengo 22 años y soy Web Developer con
-              una alta capacidad de adaptabilidad y aprendizaje, me encantan los
-              retos y manteniendo mis conocimientos en constante actualización
-              ofrezco soluciones efectivas a problemas del día a día.
+              Hola! mi nombre es Daniela, tengo 22 años y soy Web Developer
+              mexicana con una alta capacidad de adaptabilidad y aprendizaje, me
+              encantan los retos y manteniendo mis conocimientos en constante
+              actualización ofrezco soluciones efectivas a problemas del día a
+              día en la industria.
             </p>
+            <div className="container-icon">
+              <div className="card__wrap">
+              <div className="card">
+              <p className="icon__html">HTML</p>
+              </div>
+              </div>
+              <div className="card">
+              </div>
+              <div className="card">
+              <p className="icon__css">CSS</p>
+              </div>
+              <div className="card">
+              <p className="icon__js">Javascript</p>
+              <img src="JS-icon.png" alt="" />
+              </div>
+              <div className="card">
+              <p className="icon__bs">Bootstrap</p>
+              </div>
+              <div className="card">
+              <p className="icon__rs">React JS</p>
+              </div>
+            </div>
           </div>
         </article>
         <article id="proyects">
-          <p>Proyectos</p>
+          <h2>Proyectos</h2>
         </article>
         <article id="contact">
-          <p>Contacto</p>
+          <h2>Contacto</h2>
         </article>
       </section>
     </div>
